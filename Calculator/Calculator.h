05c320f914e3774826987c19a811c1073e663ca1
wangjunbao8 <615660529@qq.com>
2018-05-17 19:41:51 +0800

    wang

--- Calculator/Calculator.h
+++ Calculator/Calculator.h
@@ -6,12 +6,12 @@ class Calculator
 public:
 	int add(int num1, int num2)
 	{
-		return num1 + num1;
+		return num1 + num2;
 	}
 
 	int sub(int num1, int num2)
 	{
-		return num2 - num1;
+		return num1 - num2;
 	}
 };
 
